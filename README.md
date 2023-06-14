879a0fbb0cb55ba921183f3fc6a728b9fe96a148
sehogas <sehogas@gmail.com>
2023-06-14 15:40:58 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# WEBSOCKETS-GO
+
+Es un ejemplo básico de CHAT desarrollado en golang (servidor) y javascript (cliente).
+El servidor que implementa tecnología de websockets e incluye autenticación, websocket secure (wss), multisala, multiusuario, etc. 
+
---
+++ manager.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"sync"
+	"time"
+)
+
+// Manager is used to hold references to all Clients Registered, and Broadcasting etc
+type Manager struct {
+	clients ClientList
+
+	// Using a syncMutex here to be able to lcok state before editing clients
+	// Could also use Channels to block
+	sync.RWMutex
+	// handlers are functions that are used to handle Events
+	handlers map[string]EventHandler
+	// otps is a map of allowed OTP to accept connections from
+	otps RetentionMap
+}
+
+// NewManager is used to initalize all the values inside the manager
+func NewManager(ctx context.Context) *Manager {
+	m := &Manager{
+		clients:  make(ClientList),
+		handlers: make(map[string]EventHandler),
+		// Create a new retentionMap that removes Otps older than 5 seconds
+		otps: NewRetentionMap(ctx, 5*time.Second),
+	}
+	m.setupEventHandlers()
+	return m
+}
+
+// routeEvent is used to make sure the correct event goes into the correct handler
+func (m *Manager) routeEvent(event Event, c *Client) error {
+	// Check if Handler is present in Map
+	if handler, ok := m.handlers[event.Type]; ok {
+		// Execute the handler and return any err
+		if err := handler(event, c); err != nil {
+			return err
+		}
+		return nil
+	} else {
+		return ErrEventNotSupported
+	}
+}
+
+// setupEventHandlers configures and adds all handlers
+func (m *Manager) setupEventHandlers() {
+	m.handlers[EventSendMessage] = SendMessageHandler
+	m.handlers[EventChangeRoom] = ChatRoomHandler
+}
